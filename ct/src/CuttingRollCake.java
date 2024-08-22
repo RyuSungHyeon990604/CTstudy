f828f1f6e9213114437c135d355e60973bce7cee
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-08-22 18:26:49 +0900

    롤케이크 자르기 (배열사용) -완-

--- ct/src/CuttingRollCake.java
+++ ct/src/CuttingRollCake.java
@@ -3,20 +3,28 @@ import java.util.*;
 public class CuttingRollCake {
     public int solution(int[] topping) {
         int answer = 0;
-        Map<Integer, Integer> left = new HashMap<>();
-        Map<Integer, Integer> right = new HashMap<>();
-        for (int i : topping){
-            right.put(i,right.getOrDefault(i,0)+1);
+
+        int [] right = new int[10001];
+        int [] left  = new int[10001];
+
+        for (int i = 0; i < topping.length; i++) {
+            right[topping[i]] += 1;
+            if(right[topping[i]] == 1)
+                right[0] +=1;
         }
-        for (int i : topping){
-           right.put(i,right.getOrDefault(i,0)-1);
-           if(right.get(i)==0){
-               right.remove(i);
-           }
-           left.put(i,left.getOrDefault(i,0)+1);
-           if(left.size()==right.size()){
-               answer += 1;
-           }
+        for (int i = 0 ; i <topping.length; i++) {
+
+            right[topping[i]] -= 1;
+            if (right[topping[i]] == 0){
+                right[0] -= 1;
+            }
+
+            left[topping[i]] += 1;
+            if(left[topping[i]] == 1)
+                left[0] += 1;
+
+            if(left[0] == right[0])
+                answer++;
         }
         return answer;
     }
