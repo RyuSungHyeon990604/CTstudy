c5ff305dcd2a4eff8e61587df0993ed552c04a00
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-08-22 16:50:54 +0900

    숫자변환 bfs 하향식 -완-

--- ct/src/ChangeNumber.java
+++ ct/src/ChangeNumber.java
@@ -10,24 +10,24 @@ public class ChangeNumber {
         int answer = -1;
         int[] visit = new int[y+1];
         Queue<Integer> q = new LinkedList<>();
-        q.add(x);
+        q.add(y);
         while (!q.isEmpty()) {
             int temp = q.poll();
-            if (temp == y){
+            if (temp == x){
                 answer = visit[temp];
                 break;
             }
-            if(temp+n <= y && visit[temp+n] == 0){
-                q.add(temp + n);
-                visit[temp + n] = visit[temp] + 1;
+            if(temp - n >= x && visit[temp - n] == 0){
+                q.add(temp - n);
+                visit[temp - n] = visit[temp] + 1;
             }
-            if(temp*2 <= y && visit[temp * 2] == 0){
-                q.add(temp * 2);
-                visit[temp * 2] = visit[temp] + 1;
+            if(temp%2 == 0  && temp/2  >= x && visit[temp/2] == 0){
+                q.add(temp/2);
+                visit[temp/2] = visit[temp] + 1;
             }
-            if(temp*3 <= y && visit[temp * 3] == 0){
-                q.add(temp * 3);
-                visit[temp * 3] = visit[temp] + 1;
+            if(temp%3 == 0  && temp/3  >= x && visit[temp/3] == 0){
+                q.add(temp/3);
+                visit[temp/3] = visit[temp] + 1;
             }
 
         }
