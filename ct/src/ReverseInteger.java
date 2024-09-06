55258b185984780f1ba591e05bed14168f1270a3
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-09-06 23:26:04 +0900

    ReverseInteger (다른풀이)  -완-

--- ct/src/ReverseInteger.java
+++ ct/src/ReverseInteger.java
@@ -1,16 +1,29 @@
 import java.util.Arrays;
 
 public class ReverseInteger {
+//    public int reverse(long x) {
+//        int ans = 0;
+//        String str = String.valueOf(x < 0 ? -x : x);
+//        StringBuffer sb = new StringBuffer(str).reverse();
+//
+//        try{
+//            ans = Integer.parseInt(String.valueOf(sb));
+//        }catch(NumberFormatException e){
+//            return 0 ;
+//        }
+//        return x < 0 ? -ans : ans;
+//    }
     public int reverse(long x) {
-        int ans = 0;
-        String str = String.valueOf(x < 0 ? -x : x);
-        StringBuffer sb = new StringBuffer(str).reverse();
+        long ans = 0;
+        long n = Math.abs(x);
 
-        try{
-            ans = Integer.parseInt(String.valueOf(sb));
-        }catch(NumberFormatException e){
-            return 0 ;
+        while(n>0){
+           int mod = (int) n % 10;
+           n = n / 10;
+           ans = ans * 10 + mod;
+           if(ans > Integer.MAX_VALUE)
+               return 0;
         }
-        return x < 0 ? -ans : ans;
+        return x > 0 ? (int) ans :(int) -ans;
     }
 }
