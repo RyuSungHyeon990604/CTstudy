88b88e4863b27df146ccfa45cf921e3069bbd6dc
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-09-10 02:18:42 +0900

    Letter Combinations of a Phone Number (개선) -완-

--- ct/src/LetterCombinationsOfAPhoneNumber.java
+++ ct/src/LetterCombinationsOfAPhoneNumber.java
@@ -1,34 +1,36 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LetterCombinationsOfAPhoneNumber {
     public List<String> letterCombinations(String digits) {
         if(digits.isEmpty())
-            return new ArrayList<>();
+            return Collections.emptyList(); //빈 배열을 생성하지 않고, 자바에서 제공하는 빈 리스트를 반환하도록 수정
 
         List<String> res = new ArrayList<>();
-        char[] digit = digits.toCharArray();
+//        char[] digit = digits.toCharArray();
         char[][] phone = new char[][]{{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
-        List<char[]> phoneList = new ArrayList<>();
-
-        for(char c : digit) {
-            phoneList.add(phone[c -'0']);
-        }
-        combinations(phoneList,0,res,new StringBuilder());
+       //굳이 phoneList를 만들 필요가없음
+//        List<char[]> phoneList = new ArrayList<>();
+//
+//        for(char c : digit) {
+//            phoneList.add(phone[c -'0']);
+//        }
+        combinations(digits.toCharArray(),phone,0,res,new StringBuilder());
 
         return res;
     }
 
-    public void combinations(List<char[]> phoneList, int n,List<String> res, StringBuilder comb) {
-        if(phoneList.size() == comb.length()) {
+    public void combinations(char[] digit,char[][] phone, int n,List<String> res, StringBuilder comb) {
+        if(comb.length() == digit.length) {
             res.add(comb.toString());
             return;
         }
-       for (int i = 0; i < phoneList.get(n).length; i++) {
-           char[] phone = phoneList.get(n);
-           comb.append(phone[i]);
-           combinations(phoneList,n+1, res,comb );
-           comb.deleteCharAt(comb.length()-1);
+       for (int i = 0; i < phone[digit[n]-'0'].length; i++) {
+           comb.append( phone[digit[n]-'0'][i]);
+           combinations(digit,phone,n+1, res,comb );
+           //deleteCharAt 보다 약간더 성능이 좋음
+           comb.setLength(comb.length()-1);
        }
     }
 }
