e27c8582fc34bd8d58296064899d897c8bc20e73
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-09-21 02:37:52 +0900

    Find First and Last Position of Element in Sorted Array(개선) -완-

--- ct/src/LeetCode34.java
+++ ct/src/LeetCode34.java
@@ -1,19 +1,38 @@
 public class LeetCode34 {
     public int[] searchRange(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
+        int start = -1,end = -1;
         //non-desc 비내림차순 ==> 중복요소있을수있음
         // nums = [1,1,1,1,1] / target = 1  이라면?, nums = [0,0,1,1,1,1,1,1,1,1] / target = 1이라면?
         // 이진탐색으로 찾고 양쪽으로 뻗어나가기?
         while (left <= right) {
-
-//            if(left == right){ // 어짜피 최종 return 에 작성해놔서 필요없는부분
-//                if(nums[left]!= target)
-//                    return new int[]{-1,-1};
-//            }
-
             int mid = left+(right-left)/2;
+
             if(nums[mid] == target){
-                return new int[]{expandLeft(nums,target,mid),expandRight(nums,target,mid)};
+                left = mid + 1; //오른쪽에도 있는지 확인
+                end = mid;
+            }else{
+                if(nums[mid] > target){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+
+        }
+
+        if(start == -1){// start 값이 -1이라면 해당하는 값이 없다는 뜻
+            return new int[]{-1, -1};
+        }
+
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left+(right-left)/2;
+
+            if(nums[mid] == target){
+                right = mid - 1; //오른쪽에도 있는지 확인
+                start = mid;
             }else{
                 if(nums[mid] > target){
                     right = mid - 1;
@@ -25,39 +44,10 @@ public class LeetCode34 {
         }
         
 
-        return  new int[]{-1,-1};
+        return  new int[]{start,end};
     }
     
-    public int expandLeft(int[] nums, int target,int index) {
-        int preIndex = index;
-        if(index == 0 )
-            return index;
-        
-        while(index >= 0){
-            if(nums[index] != target){
-                break;
-            }
-            preIndex = index;
-            index--;
-        }
-        
-        return preIndex;
-    }
 
-    public int expandRight(int[] nums, int target,int index) {
-        int preIndex = index;
-        if(index == nums.length - 1 )
-            return index;
-
-        while(index <= nums.length-1){
-            if(nums[index] != target)
-                break;
-            preIndex = index;
-            index++;
-        }
-
-        return preIndex;
-    }
 
     public static void main(String[] args) {
         LeetCode34 leetcode = new LeetCode34();
