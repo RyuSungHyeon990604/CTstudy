f9bef7ace1453900224ac68a75b7acea19492fd0
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-09-23 22:32:27 +0900

    ExtraCharactersInAString : dp풀이 다시보기 -완-

--- ct/src/ExtraCharactersInAString.java
+++ ct/src/ExtraCharactersInAString.java
@@ -1,82 +1,38 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ExtraCharactersInAString {
-    class Root{
-        Trie top;
-        Root(Trie nodes){
-            this.top = nodes;
-        }
 
-        public int getExtraCharactersCount(String str){
-            int count = 0;
-            //metzeaencgpgvsckjrqafkxgyzbe
-            char[] charArray = str.toCharArray();
-            for(int i = 0; i<charArray.length; i++){
-                int j = i;
-                Trie cur = top;
-                int strLen = 0;
-                while(j<charArray.length){
-                    char c = charArray[j];
-                    boolean isContains = cur.children.containsKey(c);
-                    if(isContains){
-                        cur = cur.children.get(c);
-                        if(cur.isEnd){
-                            strLen = j - i + 1;
-                        }
-                    }else{
-                        break;
-                    }
-                    j++;
-                }
-                if(strLen > 0){
-                    i += strLen - 1;
-                }else{
-                    count++;
-                }
-
-            }
-
-
-
-            return count;
-        }
-    }
-    class Trie{
-        boolean isEnd;
-        Map<Character, Trie> children;
-
-        Trie(){
-            children = new HashMap<Character, Trie>();
-            isEnd = false;
-        }
-
-        public void insert(String word){
-            Trie cur = this;
-            char[] charArray = word.toCharArray();
-            for(int i = 0; i < charArray.length ;i++ ){
-                cur.children.putIfAbsent(charArray[i], new Trie());
-                cur= cur.children.get(charArray[i]);
-            }
-            cur.isEnd = true;
-
-        }
-
-    }
 
     public int minExtraChar(String s, String[] dictionary) {
-        Trie trie = new Trie();
-        Root root = new Root(trie);
-        for (String str : dictionary) {
-            root.top.insert(str);
-        }
-        return root.getExtraCharactersCount(s);
+       int res = 0;
+       Set<String> dic = new HashSet<String>();
+       for (String d : dictionary) {
+           dic.add(d);
+       }
+       int[] dp = new int[s.length() + 1];
+       for (int i = 1; i <= s.length(); i++) {
+           dp[i] = i;
+       }
 
+       for (int i = 1; i <= s.length(); i++) {
+           for (int j= 0 ; j< i ;j++){
+               String sub = s.substring(j,i);
+               if(dic.contains(sub)){//j == 0일때 사전에 존재하는것은 여분문자가 0임 / 0이 아닐때라면 문자는 dp[j] + 추가 문자의 갯수
+                   dp[i] = Math.min(dp[i], dp[j]);
+               }else{
+                   dp[i] = Math.min(dp[i], dp[j]+i-j );
+               }
+           }
+       }
+       return dp[dp.length-1];
     }
 
     public static void main(String[] args) {
         ExtraCharactersInAString ex = new ExtraCharactersInAString();
-        ex.minExtraChar("metzeaencgpgvsckjrqafkxgyzbe",new String[]{"zdzz","lgrhy","r","ohk","zkowk","g","zqpn","anoni","ka","qafkx","t","jr","xdye","mppc","bqqb","encgp","yf","vl","ctsxk","gn","cujh","ce","rwrpq","tze","zxhg","yzbe","c","o","hnk","gv","uzbc","xn","kk","ujjd","vv","mxhmv","ugn","at","kumr","ensv","x","uy","gb","ae","jljuo","xqkgj"});
+        ex.minExtraChar("abc",new String[]{"bc"});
     }
 
 }
