01c3f275eb731455db5360035ded4a7bec32eea6
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-09-26 20:56:13 +0900

    729. My Calendar I (개선) -완-

--- ct/src/MyCalendar.java
+++ ct/src/MyCalendar.java
@@ -1,21 +1,45 @@
 import java.util.*;
 
 public class MyCalendar {
-    List<int[]> calendar;
+    TreeMap<Integer,Integer> map = new TreeMap<>();
 
     public MyCalendar() {
-        calendar = new ArrayList<>();
+
     }
 
     public boolean book(int start, int end) {
 
-        for (int[] c : calendar){
-            if(start <= c[0] && end >= c[1]) return false;
-            if(end < c[1] && end >= c[0]) return false;
-            if(start <= c[1] && start > c[0]) return false;
-        }
-        calendar.add(new int[]{start, end});
+        // (1 : 100) <-- return  (start : end)  (250:260)
 
-        return true;
+        Map.Entry<Integer, Integer> a = map.floorEntry(start);
+        Map.Entry<Integer, Integer> b = map.ceilingEntry(start);
+        if(a==null && b==null) {
+            map.put(start, end);
+            return true;
+        }else if(a==null || b==null){
+            if(a == null &&  end <= b.getKey()){
+                map.put(start, end);
+                return true;
+            }
+            if(b == null && start >= a.getValue()){
+                map.put(start, end);
+                return true;
+            }
+        }else{
+            if(start >= a.getValue() && end <= b.getKey()){
+                map.put(start, end);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        MyCalendar mc = new MyCalendar();
+        mc.book(37,50);
+        mc.book(33,50);
+        mc.book(4,17);
+        mc.book(35,48);
+        mc.book(8,25);
     }
 }
