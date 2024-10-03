121efa75764050495b4456344f65595927487aa7
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-10-03 22:51:41 +0900

    Make Sum Divisible by P : 모듈러연산을이용한 풀이, 조금더 이해할 필요있음 -완-

--- ct/src/MakeSumDivisibleByP.java
+++ ct/src/MakeSumDivisibleByP.java
@@ -1,30 +1,41 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MakeSumDivisibleByP {
     public int minSubarray(int[] nums, int p) {
         int res = nums.length;
-        long[] sum = new long[nums.length+1];;
-        for (int i = 1; i <= nums.length; i++) {
-            sum[i] = sum[i-1] + nums[i-1];
+        long sum = 0;
+        Map<Long, Long> map = new HashMap<>();
+        for (int i : nums) {
+            sum += i;
         }
-        if(sum[sum.length-1] % p == 0)
-            return 0;
-        for (int start = 1; start <= nums.length; start++) {
-            for (int end = start; end <= nums.length; end++) {
-                long subSum = sum[end] - sum[start-1];
-                if((sum[sum.length-1]-subSum) % p == 0) {
-                    res = Math.min(res,(end-start+1));
-                    if(res == 1)
-                        return res;
-                }
-            }
+        long rem = sum % p; // 상쇄햐아하는 나머지
+        if (rem == 0) return 0;
+        long subSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            //subSum mod p = subRem , preSubSum mod p = remRem , totalSum mod p = rem 일때
+            //subSum = sum[0,i] preSubSum = [0,n] (0<=n<=i) 일때
+            //(subSum - preSubSum) mod p = rem 이되는 remRem을 구해야한다
+            //(subSum - preSubSum) mod p = (subSum mod p - preSubSum mod p ) mod p = (subRem - remRem) mod p = rem 이고
+            // 결국 remRem = subRem - rem 이다 (remRem < 0 이라면 p를 더해주어야한다)
+            // 즉 이전에 remRem 값과 동일한 subRem이 나온 i 값이 있다면 [preI,curI] 까지범위가 sum[preI,curI] mod p = rem이게 된다.
+            // 즉 [preI,curI]가 이 문제에서 구하는 subArry의 길이다.
+            subSum+=nums[i];
+            long subRem = subSum%p;
+            long remRem  = subRem - rem;
+            if (map.containsKey(remRem))
+                res = (int) Math.min(res, i - map.get(remRem));
+
+            map.put(subRem, (long)i); // i까지 누적합의 나머지
         }
+
 
         return res == nums.length ? -1 : res;
     }
 
     public static void main(String[] args) {
         MakeSumDivisibleByP ms = new MakeSumDivisibleByP();
-        ms.minSubarray(new int[]{1,2,3},  7);
+        ms.minSubarray(new int[]{3, 1, 4, 2},  6);
     }
 }
