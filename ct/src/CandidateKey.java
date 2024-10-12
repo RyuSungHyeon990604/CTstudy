1350261859c772b9d0dae96f1632cc9edb96ce3f
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-10-13 00:39:08 +0900

    후보키 : 후보키를 비트마스크 방식으로 조합함,1<<n == 2^n-완-

--- ct/src/CandidateKey.java
+++ ct/src/CandidateKey.java
@@ -1,58 +1,87 @@
 import java.util.*;
 
+import java.util.*;
+
 public class CandidateKey {
     int answer = 0;
-    Set<Set<Integer>> keySet =new HashSet<>();
+    List<Integer> keySet = new ArrayList<>(); // 후보키 집합
+
     public int solution(String[][] relation) {
-        List<Set<Integer>> keys = new ArrayList<>();
-        key(relation,new HashSet<>(),0,0,keys);
-        Collections.sort(keys,Comparator.comparingInt(Set::size));
-        keys.remove(0);
-        for (Set<Integer> key : keys) {
-            boolean isKey = true;
-            Set<String> db =new HashSet<>();
-            for(int j=0;j<relation.length;j++) {
-                StringBuilder sb = new StringBuilder();
-                for (int i : key) {
-                    sb.append(relation[j][i]).append(',');
+        int rowCount = relation.length;
+        int colCount = relation[0].length;
+
+        // 1. 모든 칼럼 조합에 대한 비트마스크 부분집합 생성
+        for (int subset = 1; subset < (1 << colCount); subset++) {
+            // 2. 현재 조합이 후보키인지 검사
+            if (isUnique(relation, rowCount, colCount, subset)) {
+                boolean isMinimal = true;
+                // 3. 최소성 검사 (이미 등록된 후보키와 비교)
+                for (int key : keySet) {
+                    if ((key & subset) == key) {
+                        isMinimal = false;
+                        break;
+                    }
                 }
-                if(!db.add(sb.toString())){
-                    isKey = false;
-                    break;
-                }
-            }
-            if(isKey){
-                if(chk(key)){
+                // 4. 최소성을 만족하면 후보키로 등록
+                if (isMinimal) {
+                    keySet.add(subset);
                     answer++;
-                    keySet.add(key);
                 }
             }
         }
-
         return answer;
     }
 
-    public void key(String[][] relation, Set<Integer> key,int depth,int start,List<Set<Integer>> list){
+    // 각 조합이 유일성을 만족하는지 확인
+    private boolean isUnique(String[][] relation, int rowCount, int colCount, int subset) {
+        Set<String> seen = new HashSet<>(); // 중복 검사에 사용
 
-        list.add(new HashSet<>(key));
-        for(int i=start;i<relation[0].length;i++){
-            key.add(i);
-            key(relation, key, depth + 1, i + 1,list);  // i+1을 통해 중복된 인덱스를 피함
-            key.remove(i);
-        }
-    }
-    public boolean chk(Set<Integer> key){
-        for (Set<Integer> set : keySet) {
-            if(key.containsAll(set)){
+        // 각 row를 순회하며, subset 조합에 해당하는 컬럼 값을 하나의 문자열로 결합
+        for (int i = 0; i < rowCount; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < colCount; j++) {
+                if ((subset & (1 << j)) != 0) { // subset에 해당하는 컬럼만 추가
+                    sb.append(relation[i][j]).append(",");
+                }
+            }
+            // 이미 존재하는 조합이면 유일성 실패
+            if (!seen.add(sb.toString())) {
                 return false;
             }
         }
-        return true;
+        return true; // 유일성을 만족함
+    }
+
+    public void p(){
+        int[] a = {1,2,3};
+        int n = a.length;
+
+        for (int i = 1; i < (1 << n); i++) {
+            // 각 부분집합 출력
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {//1 << j ==> 이진수로 나타냈을때 j번째가 1인 수이다 j = 0 / 1 << j = 1,j = 1 / 1 << j = 10
+                    //따라서 i=5라면 101이라면
+                    //1 << j 가 100 일때 그리고 1일때
+                    //즉 j =0 , j = 2일때이다
+                    System.out.print(a[j] + " ");
+                }
+            }
+            System.out.println(); // 한 부분집합이 끝날 때 줄바꿈
+        }
     }
 
     public static void main(String[] args) {
         CandidateKey candidateKey = new CandidateKey();
-        candidateKey.solution(new String[][]{{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}});
-    }
+        System.out.println(candidateKey.solution(new String[][]{
+                {"100", "ryan", "music", "2"},
+                {"200", "apeach", "math", "2"},
+                {"300", "tube", "computer", "3"},
+                {"400", "con", "computer", "4"},
+                {"500", "muzi", "music", "3"},
+                {"600", "apeach", "music", "2"}
+        })); // 출력: 2
 
+        candidateKey.p();
+    }
 }
+
