fabef7867d3e0527b09a7a296bfb14669c4a8927
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-10-25 19:21:20 +0900

    Remove Sub-Folders from the Filesystem : Trie 자료구조 풀이, 문자열배열을 정렬할때 길이를기준으로 정렬하는게 훨씬 빠르다.  -완-

--- ct/src/RemoveSubFoldersfromtheFilesystem.java
+++ ct/src/RemoveSubFoldersfromtheFilesystem.java
@@ -2,20 +2,65 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class RemoveSubFoldersfromtheFilesystem {
-    public List<String> removeSubfolders(String[] folder) {
-        Arrays.sort(folder);
-        List<String> res= new ArrayList<String>();
-        for (String s : folder) {
-            if(res.isEmpty()){
-                res.add(s);
-            }else{
-                if(s.startsWith(res.get(res.size()-1)+"/"))
-                    continue;
-                res.add(s);
+
+class Trie{
+    Trie[] child = new Trie[27];
+    boolean isLast = false;
+}
+
+class Root{
+    Trie root = new Trie();
+    public void insert(String path){
+        Trie current = root;
+        for(char c : path.toCharArray()){
+            int i = c - 'a';
+            if(c == '/'){
+                i = 26;
+            }
+            if(current.child[i] == null){
+                current.child[i] = new Trie();
+            }
+            current = current.child[i];
+        }
+        //  /a/b/c  /a/b/ca 같은 경우를 대비해서
+        //마지막에 문자 '/'를 하나 추가해준다
+        current.child[26] = new Trie();
+        current.child[26].isLast = true;
+    }
+    public boolean isSubfolder(String path){
+        Trie current = root;
+        for(char c : path.toCharArray()){
+            int i = c - 'a';
+            if(c == '/'){
+                i = 26;
+            }
+            if(current.child[i] == null) return false; // 다음 루트가 없다면 서브폴더가 아니다
+            current = current.child[i];
+            if(current.isLast){//완성된 경로가있다면 서브폴더이다
+                return true;
             }
 
         }
+        return false;
+    }
+}
+public class RemoveSubFoldersfromtheFilesystem {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder,(s1,s2) -> s1.length() - s2.length());
+        List<String> res= new ArrayList<String>();
+        Root root = new Root();
+        for(String s : folder){
+            if(root.isSubfolder(s)){
+                continue;
+            }
+            root.insert(s);
+            res.add(s);
+        }
         return res;
+    }
+
+    public static void main(String[] args) {
+        RemoveSubFoldersfromtheFilesystem f = new RemoveSubFoldersfromtheFilesystem();
+        f.removeSubfolders(new String[]{"/a","/a/b","/c/d","/c/d/e","/c/f"});
     }
 }
