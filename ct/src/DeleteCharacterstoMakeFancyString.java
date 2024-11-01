35338bf1a77f0e0d58688d91742ca7fdbe0cee2a
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-01 14:02:15 +0900

    Delete Characters to Make Fancy String : 빈도수 체크 방식 -완-

--- ct/src/DeleteCharacterstoMakeFancyString.java
+++ ct/src/DeleteCharacterstoMakeFancyString.java
@@ -2,16 +2,23 @@ public class DeleteCharacterstoMakeFancyString {
     public String makeFancyString(String s) {
         if(s.length() < 3)
             return s;
+        int freq = 1;
         StringBuilder answer = new StringBuilder();
         char[] charArray = s.toCharArray();
-        for (int i = 0; i < charArray.length-2; i++) {
-            if(charArray[i] == charArray[i+1] && charArray[i+2] == charArray[i] ) {
-                continue;
+        char prev = charArray[0];
+        answer.append(prev);
+        for (int i = 1; i < charArray.length; i++) {
+            if(charArray[i] == prev)
+                freq++;
+            else{
+                prev = charArray[i];
+                freq = 1;
             }
-            answer.append(charArray[i]);
+            if(freq < 3)
+                answer.append(prev);
+
         }
-        answer.append(charArray[charArray.length-2]);
-        answer.append(charArray[charArray.length-1]);
+
         return answer.toString();
     }
 }
