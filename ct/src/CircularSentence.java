f5d4de4760456485d8a51ae762f08e4fa0040381
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-02 17:43:08 +0900

    Circular Sentence : indexOf 방식, 성능개선 -완-

--- ct/src/CircularSentence.java
+++ ct/src/CircularSentence.java
@@ -1,23 +1,18 @@
 public class CircularSentence {
     public boolean isCircularSentence(String sentence) {
-        String[] split = sentence.split(" ");
-        String first = split[0];
-        if(split.length == 1) {
-            if(first.charAt(0) == first.charAt(first.length() - 1)) {
-                return true;
-            }
+        if(sentence.isEmpty())
+            return true;
+        if(sentence.charAt(0) != sentence.charAt(sentence.length()-1)) {
             return false;
         }
-        for (int i = 1; i < split.length; i++) {
-            if(split[i].charAt(0) != split[i-1].charAt(split[i-1].length() - 1))
+        int i = sentence.indexOf(" ");
+        while(i != -1) {
+            if(sentence.charAt(i-1) != sentence.charAt(i+1)) {
                 return false;
-            if(i == split.length - 1) {
-                if(split[i].charAt(split[i].length() - 1) != first.charAt(0)){
-                    return false;
-                }
             }
-
+            i = sentence.indexOf(" ", i+1);
         }
+
         return true;
     }
 }
