5a32ffd2d79cc39692f8d9fd4e87919f516da130
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-06 14:26:51 +0900

    Find if Array Can Be Sorted : 비트수에따라 그룹을 나누고 현재 그룹의 최솟값이 이전그룹의 최댓값보다 크다면 그룹간의 스왑이 발생하지않는다.  -완-

--- ct/src/FindifArrayCanBeSorted.java
+++ ct/src/FindifArrayCanBeSorted.java
@@ -1,20 +1,29 @@
 public class FindifArrayCanBeSorted {
     public boolean canSortArray(int[] nums) {
-        int n = nums.length;
-        for (int i = n-1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if(nums[j] > nums[j+1]){
-                    if(Integer.bitCount(nums[j]) == Integer.bitCount(nums[j+1])){
-                        int temp = nums[j];
-                        nums[j] = nums[j+1];
-                        nums[j+1] = temp;
-                    }else{
-                        return false;
-                    }
-                }
+        int prevMax = Integer.MIN_VALUE; // 이전 비트 그룹의 최대값
+        int currMax = nums[0]; // 현재 비트 그룹의 최대값
+        int currMin = nums[0]; // 현재 비트 그룹의 최소값
+        int setBits = Integer.bitCount(nums[0]); // 현재 비트 수
+
+        for(int i = 1; i < nums.length; i++) {
+            // 현재 숫자의 비트 수가 setBits와 같으면 현재 그룹 내에서 최대/최소값 갱신
+            if (setBits == Integer.bitCount(nums[i])) {
+                currMax = Math.max(currMax, nums[i]);
+                currMin = Math.min(currMin, nums[i]);
+            } else {
+                // 현재 그룹의 최소값이 이전 그룹의 최대값보다 작다면 정렬 불가능
+                if (currMin < prevMax)
+                    return false;
+
+                // 새로운 그룹으로 이동
+                prevMax = currMax;
+                setBits = Integer.bitCount(nums[i]);
+                currMin = nums[i];
+                currMax = nums[i];
             }
         }
-        return true;
+        // 마지막 체크: 모든 그룹을 통과했을 때, 마지막 그룹의 최소값이 이전 그룹의 최대값보다 크면 true 반환
+        return currMin > prevMax;
     }
 
     public static void main(String[] args) {
