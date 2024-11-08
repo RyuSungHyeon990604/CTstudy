b398fb3148a87b53963a42313f49a0cc1060a07c
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-08 14:58:13 +0900

    Maximum XOR for Each Query : Bit Manipulation, XOR 연산의 특징, 모든비트가 1일때 XOR연산은 모든 비트를 반전시킨다.   -완-

--- ct/src/MaximumXORforEachQuery.java
+++ ct/src/MaximumXORforEachQuery.java
@@ -1,25 +1,19 @@
 public class MaximumXORforEachQuery {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
+        //nums에는 2^maximumBit 미만의 수가 들어있으므로
+        //nums의 요소들을 XOR연산을 한다면 최댓값으로는 반드시 2^maximumBit-1 가 나올수있다
+        //때문에 nums[0] XOR... XOR nums[n] ^ k == 2^maximumBit-1 가 되는 k값은
+        //nums[0] XOR... XOR nums[n] XOR ^ 2^maximumBit-1 == k 이다
+        //2^maximumBit-1 의 모든 비트는 1이기때문에 XOR 연산시 모든 비트를 반전시킨다
+        //nums[0] XOR... XOR nums[n] ^ k 의 모든 비트가 1이 나오려면  nums[0] XOR... XOR nums[n] 과 k 의 비트가 서로 반전되어있어야하므로
+        //k 는 nums[0] XOR... XOR nums[n]의 비트를 모두 반전시키면된다
+        //때문에  k == nums[0] XOR... XOR nums[n]^(2^maximumBit-1) 이다
         int[] answer = new int[nums.length];
         int s = nums[0];
         for (int i = 0; i <nums.length; i++) {
             if(i>0)
                 s= s^nums[i];
-            int t = s;
-            int n = (1<<maximumBit) - 1;
-            int idx = 0;
-            if(t == n){
-                answer[nums.length-1-i] = 0;
-                continue;
-            }
-            while (t != 0) {
-                if((t&1) == 1) {
-                    n = n - (1 << idx);
-                }
-                idx++;
-                t = t>>1;
-            }
-            answer[nums.length-1-i] = n;
+            answer[nums.length-1-i] = s ^((1<<maximumBit)-1);
         }
         return answer;
     }
