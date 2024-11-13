46d0a1317347147869388498abe162ecb7fc88fa
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-13 14:26:33 +0900

    Count the Number of Fair Pairs : Two Pointer 방식 -완-

--- ct/src/CounttheNumberofFairPairs.java
+++ ct/src/CounttheNumberofFairPairs.java
@@ -2,38 +2,28 @@ import java.util.Arrays;
 
 public class CounttheNumberofFairPairs {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        long answer = 0L;
         Arrays.sort(nums);
-        for(int i = 0 ; i < nums.length-1 ; i++) {
-            int left = i, right = nums.length - 1;
-            int targetLower = lower - nums[i];
-            int targetUpper = upper - nums[i];
-            int upperIdx = i;
-            int lowerIdx = i;
-
-            while(left <= right) {
-                int mid = (left + right) / 2;
-                if(nums[mid] <= targetUpper) {
-                    left = mid + 1;
-                    upperIdx =mid;
-                }else{
-                    right = mid - 1;
-                }
+        int left = 0, right = nums.length - 1;
+        long upperCount = 0;
+        long lowerCount = 0;
+        while (left < right) {
+            if(nums[left] + nums[right] <= upper) {
+                upperCount += right - left;
+                left++;
+            }else{
+                right--;
             }
-            left = i;
-            right = nums.length - 1;
-            while(left <= right) {
-                int mid = (left + right) / 2;
-                if(nums[mid] < targetLower) {
-                    left = mid + 1;
-                    lowerIdx = mid;
-                }else{
-                    right = mid - 1;
-                }
-            }
-            answer += upperIdx - lowerIdx;
         }
-
-        return answer;
+        left = 0;
+        right = nums.length - 1;
+        while (left < right) {
+            if(nums[left] + nums[right] <= lower-1) {
+                lowerCount += right - left;
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return upperCount - lowerCount;
     }
 }
