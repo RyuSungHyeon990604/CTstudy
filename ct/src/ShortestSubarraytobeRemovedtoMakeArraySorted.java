792aa4f260af135c2a9129c78fcf78fee7b04576
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-15 18:41:15 +0900

    코드, 주석 수정   -완-

--- ct/src/ShortestSubarraytobeRemovedtoMakeArraySorted.java
+++ ct/src/ShortestSubarraytobeRemovedtoMakeArraySorted.java
@@ -14,15 +14,15 @@ public class ShortestSubarraytobeRemovedtoMakeArraySorted {
             right--;
         }
 
-        int result = Math.min(n - left - 1, right);// 왼쪽부분과 오른쪽부분만을 남겻을때 지워지는 부분수열의 최소길이
+        int result = Math.min(n - left - 1, right);// 왼쪽부분이나 오른쪽부분만을 남겻을때 지워지는 부분수열의 최소길이
 
-        int i = 0, j = right;
-        while (i <= left && j < n) {//i의 최댓값은 left부분의 오름차순의 마지막 인덱스 즉, left
-            if (arr[i] <= arr[j]) {
-                result = Math.min(result, j - i - 1);
+        int i = 0;
+        while (i <= left && right < n) {//i가 left를 넘어가면 비내림차순이 끝나버림, right부터 n까지는 비내림차순
+            if (arr[i] <= arr[right]) {
+                result = Math.min(result, right - i - 1);
                 i++;// 더 작은 부분배열이 있는지 탐색
-            } else {
-                j++;//arr[i] > arr[j] 인 j값을 삭제하는 부분배열에 포함시킨다
+            } else {//만약 arr[i]가 arr에서 가장 큰 값이라면 right는 n이된다.
+                right++;//arr[i] > arr[j] 인 j값을 삭제하는 부분배열에 포함시킨다
             }
         }
 
