2db115f9e47c46283f5e299f2e43da9651ddd080
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-18 11:11:32 +0900

    Defuse the Bomb : Sliding Window, 성능개선 -완-

--- ct/src/DefusetheBomb.java
+++ ct/src/DefusetheBomb.java
@@ -2,18 +2,25 @@ public class DefusetheBomb {
     public int[] decrypt(int[] code, int k) {
         int[] answer = new int[code.length];
         if( k == 0) return answer;
-        for (int i = 0 ; i < code.length ; i++){
-           int sum = 0;
-           if(k > 0){
-               for(int j = i+1 ; j <= i+k ; j++){
-                   sum += code[j%code.length];
-               }
-           }else{
-               for(int j = i-1 ; j >= i+k ; j--){
-                   sum += code[(j < 0 ? code.length+j : j)%code.length];
-               }
-           }
-           answer[i] = sum;
+        int start = 1;
+        int end = k;
+        if( k < 0){
+            start = code.length + k;
+            end = code.length - 1;
+        }
+        int sum = 0;
+        //일일히 sum을 초기화하고 값을 더할필요가없음
+        for (int i = start; i <= end; i++) {
+            sum += code[i];
+        }
+        //어짜피 이동은 한칸씩 고정
+        //즉, sum 에서 code[start] 를 빼고 code[end+1]을 더해주면  answer[i+1] 의 값이 나온다
+        for (int i = 0; i <= end; i++) {
+            answer[i] = sum;
+            sum -= code[start%code.length];
+            sum += code[(end+1)%code.length];
+            start++;
+            end++;
         }
         return answer;
     }
