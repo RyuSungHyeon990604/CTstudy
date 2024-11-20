41255c5d0a4cecf55e02d0a82c878f0826399340
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-21 00:03:42 +0900

    2개 이하로 다른 비트 : 비트연산 풀이, 성능개선 완-

--- ct/src/DifferentBit.java
+++ ct/src/DifferentBit.java
@@ -4,47 +4,28 @@ import java.util.List;
 public class DifferentBit {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-        int idx = 0;
         for(int i =0;i < numbers.length;i++) {
-            if(numbers[i] % 2 == 0){// 짝수라면 가장마지막비트가 0 이므로 +1만 해준다
+            if(numbers[i] % 2 == 0){
                 answer[i] = numbers[i]+1;
-            }else{//홀수라면 가장 마지막으로나오는 01을 10으로 바꿔준다
-                long number = numbers[i];
-                boolean flag = true;
-                List<Long> bitArr = new ArrayList<>();//list에는 거꾸로들어가므로 가장먼저 들어가는 10을 찾아 값의 인덱스를 바꾼다
-                while(number != 0){
-                    bitArr.add(number%2);
-                    if(bitArr.size()>=2){
-                        if(flag && bitArr.get(bitArr.size()-2)==1 && bitArr.get(bitArr.size()-1)==0){
-                            bitArr.set(bitArr.size()-2,0L);
-                            bitArr.set(bitArr.size()-1,1L);
-                            flag = false;
-                        }
+            }else{
+                long t = numbers[i];
+                int bit = 1;
+                while (t != 0){
+                    if((t&1) != 1){
+                        break;
                     }
-                    number = number / 2;
+                    t = t >> 1;
+                    bit++;
                 }
-                if(flag){
-                    bitArr.set(bitArr.size()-1, 0L);
-                    bitArr.add(1L);
-                }
-                long n=0;
-                for(int j = 0;j < bitArr.size();j++){
-                    if(bitArr.get(j) == 1)
-                        n = (long) (n + Math.pow(2,j));
-                }
-                answer[i] = n;
+                answer[i] = numbers[i] + (1L<<(bit-1)) - (1L<<(bit-2));
             }
         }
-
         return answer;
     }
 
-    public void sum(){
-        int sum = 0;
-        for(int i = 1 ;i<=100;i++){
-            sum = sum + i;
-        }
+    public static void main(String[] args) {
+        DifferentBit differentBit =new DifferentBit();
+        differentBit.solution(new long[]{5,127});
     }
-
 
 }
