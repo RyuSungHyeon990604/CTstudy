7c268717222c254ad23192cf113a1b9b2b7cbaca
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-11-28 15:04:13 +0900

    Minimum Obstacle Removal to Reach Corner : BFS, 다익스트라, Deque -완-

--- ct/src/MinimumObstacleRemovaltoReachCorner.java
+++ ct/src/MinimumObstacleRemovaltoReachCorner.java
@@ -1,25 +1,23 @@
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class MinimumObstacleRemovaltoReachCorner {
     public int minimumObstacles(int[][] grid) {
-        return bfs(grid);
+        return bfsWithDeque(grid);
     }
     public int bfs(int[][] grid){
         int[] dirX = {1,-1,0,0};
         int[] dirY = {0,0,1,-1};
         int m = grid.length;
         int n = grid[0].length;
-        int[][] vis = new int[m][n];
+        int[][] obstacles = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                vis[i][j] = m*n;
+                obstacles[i][j] = m*n;
             }
         }
         Queue<int[]> pq = new PriorityQueue<>((o1,o2) -> o1[2] - o2[2]);
         pq.offer(new int[]{0,0,0});
-        vis[0][0] = 0;
+        obstacles[0][0] = 0;
         while(!pq.isEmpty()){
             int size = pq.size();
             for(int i = 0; i < size; i++){
@@ -31,18 +29,53 @@ public class MinimumObstacleRemovaltoReachCorner {
                     int nx = x + dirX[j];
                     int ny = y + dirY[j];
                     if(nx > m-1 || ny > n-1 || nx < 0 || ny < 0) continue;
-                    if(grid[nx][ny]==1 && vis[nx][ny] > remv+1){
-                        vis[nx][ny] = remv+1;
-                        pq.offer(new int[]{nx, ny, remv + 1});
-                    }
-                    if(grid[nx][ny]==0 && vis[nx][ny] > remv){
-                        vis[nx][ny] = remv;
-                        pq.offer(new int[]{nx, ny, remv});
+                    if(obstacles[nx][ny] > remv+grid[nx][ny]){
+                        obstacles[nx][ny] = remv+grid[nx][ny];
+                        pq.offer(new int[]{nx, ny, remv + grid[nx][ny]});
                     }
                 }
             }
         }
-        return vis[m-1][n-1];
+        return obstacles[m-1][n-1];
+    }
+
+    public int bfsWithDeque(int[][] grid){
+        int[] dirX = {1,-1,0,0};
+        int[] dirY = {0,0,1,-1};
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] obstacles = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                obstacles[i][j] = m*n;
+            }
+        }
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.addLast(new int[]{0,0,0});
+        obstacles[0][0] = 0;
+        while(!dq.isEmpty()){
+            int size = dq.size();
+            for(int i = 0; i < size; i++){
+                int[] poll = dq.pollFirst();
+                int x = poll[0];
+                int y = poll[1];
+                int remv = poll[2];
+                for(int j = 0; j < 4; j++){
+                    int nx = x + dirX[j];
+                    int ny = y + dirY[j];
+                    if(nx > m-1 || ny > n-1 || nx < 0 || ny < 0) continue;
+                    if(grid[nx][ny] == 1 && obstacles[nx][ny] > remv+grid[nx][ny]){
+                        obstacles[nx][ny] = remv+grid[nx][ny];
+                        dq.addLast(new int[]{nx, ny, remv + grid[nx][ny]});
+                    }
+                    if(grid[nx][ny] == 0 && obstacles[nx][ny] > remv+grid[nx][ny]){
+                        obstacles[nx][ny] = remv+grid[nx][ny];
+                        dq.addFirst(new int[]{nx, ny, remv + grid[nx][ny]});
+                    }
+                }
+            }
+        }
+        return obstacles[m-1][n-1];
     }
     public static void main(String[] args) {
         MinimumObstacleRemovaltoReachCorner m = new MinimumObstacleRemovaltoReachCorner();
