9d0df3aa39664ac6ebb81a9281393cd98cbc998f
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-12-01 17:38:20 +0900

    Check If N and Its Double Exist : Set -완-

--- ct/src/LEETCODE/CheckIfNandItsDoubleExist/Solution.java
+++ ct/src/LEETCODE/CheckIfNandItsDoubleExist/Solution.java
@@ -1,41 +1,21 @@
 package LEETCODE.CheckIfNandItsDoubleExist;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
-        for (int i = 0 ; i < arr.length ; i++) {
-            int target = arr[i]*2;
-            if(target > arr[arr.length-1]){
-                return false;
+        Set<Integer> set = new HashSet<>();
+        for(int i : arr){
+            if(set.contains(i*2) || (i % 2 ==0 && set.contains(i/2))){
+                return true;
             }
-            int left = i, right = arr.length - 1;
-            if(target < 0){
-                left = 0;
-                right = i;
-
-            }
-            while(left <= right){
-                int mid = (left + right)/2;
-                if(arr[mid] == target && mid != i){
-                    return true;
-                }else{
-                    if(arr[mid] > target){
-                        right = mid - 1;
-                    }else{
-                        left = mid + 1;
-                    }
-                }
-            }
+            set.add(i);
         }
         return false;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.checkIfExist(new int[]{-766,259,203,601,896,-226,-844,168,126,-542,159,-833,950,-454,-253,824,-395,155,94,894,-766,-63,836,-433,-780,611,-907,695,-395,-975,256,373,-971,-813,-154,-765,691,812,617,-919,-616,-510,608,201,-138,-669,-764,-77,-658,394,-506,-675,523});
+        boolean b = solution.checkIfExist(new int[]{-10, 12, -20, -8, 15});
     }
 }
