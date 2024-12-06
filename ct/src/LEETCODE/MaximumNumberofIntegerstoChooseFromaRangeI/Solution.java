f548ca890e2de8ec278a76d97cb59b73dae00b46
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-12-06 14:41:17 +0900

    Maximum Number of Integers to Choose From a Range I :성능개선 -완-

--- ct/src/LEETCODE/MaximumNumberofIntegerstoChooseFromaRangeI/Solution.java
+++ ct/src/LEETCODE/MaximumNumberofIntegerstoChooseFromaRangeI/Solution.java
@@ -5,37 +5,19 @@ import java.util.Arrays;
 public class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
         int answer = 0;
-        int m =banned.length;
-        int[] range = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            range[i] = i;
-        }
-        for (int i = m-1; i >= 0; i--) {
-            int num = banned[i];
-            if(num <= n) {
-                range[num] = 0;
-            }
-        }
-        Arrays.sort(range);
-        int left = 0,right = n;
-        int zeroIndex = 0;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if(range[mid] > 0) {
-                right = mid - 1;
-            }else{
-                if(range[mid] == 0)
-                    zeroIndex = mid;
-                left = mid + 1;
-            }
+        int[] range = new int[10001];
+        for (int num : banned) {
+            range[num] = 1;
         }
         int sum = 0;
-        for (int i = zeroIndex+1; i <= n; i++) {
-            sum += range[i];
-            answer++;
-            if(sum > maxSum){
-                answer--;
-                break;
+        for (int i = 1; i <= n; i++) {
+            if(range[i] == 0){
+                sum += i;
+                answer++;
+                if(sum > maxSum){
+                    answer--;
+                    break;
+                }
             }
         }
 
