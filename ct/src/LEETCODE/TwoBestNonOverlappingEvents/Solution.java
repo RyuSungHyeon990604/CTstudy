c226e75e8ca7f6c0d8146ad54971b09fde25df0e
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-12-08 20:08:23 +0900

    Two Best Non-Overlapping Events : Sorting, Binary Search -완-

--- ct/src/LEETCODE/TwoBestNonOverlappingEvents/Solution.java
+++ ct/src/LEETCODE/TwoBestNonOverlappingEvents/Solution.java
@@ -12,26 +12,39 @@ import java.util.*;
 //Number of Flowers in Full Bloom -> 2251
 public class Solution {
     public int maxTwoEvents(int[][] events) {
+        int max = Integer.MIN_VALUE;
+        int n = events.length;
         Arrays.sort(events, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
-
-        //종료시점을 기준으로 정렬하므로 가장 빨리 끝나는 이벤트부터 poll됨
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-
-        pq.offer(events[0]);
-        int max = 0;
-        int ans = 0;
-
-        for(int i = 0; i < events.length; i++) {
-            while(!pq.isEmpty() && pq.peek()[1] < events[i][0]) {
-                //현재의 이벤트이전에 종료된 이벤트트들 중에서 가장 큰 가중치를 가진 이벤트를 max값으로 설정
-                max = Math.max(max, pq.poll()[2]);
+        int[] suffixArr = new int[n];
+        suffixArr[n-1] = events[n-1][2];
+        //i번째 이벤트 이후에 나올수있는 최대 value
+        for (int i = n-2; i >= 0; i--) {
+            suffixArr[i] = Math.max(events[i][2], suffixArr[i+1]);
+        }
+        for (int i = 0; i < n; i++) {
+            int left = i, right = n-1;
+            int nextEvt = -1;
+            //현재 인벤트 종료후에 나오는 다음 이벤트의 위치를 찾는다. 즉 겹치지않는 다음이벤트
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (events[mid][0] > events[i][1]) {
+                    right = mid-1;
+                    nextEvt = mid;
+                }else{
+                    left = mid+1;
+                }
             }
-
-            ans = Math.max(ans, max + events[i][2]);
-            pq.offer(events[i]);
+            if (nextEvt == -1) {//다음 이벤트가 없다면
+                //현재 이벤트의 value를 최댓값
+                max = Math.max(max, events[i][2]);
+            }else{
+                max = Math.max(max, events[i][2] + suffixArr[nextEvt]);
+            }
         }
 
-        return ans;
+
+
+        return max;
     }
 
 
