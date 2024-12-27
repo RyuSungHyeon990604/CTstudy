19c094b1cee70269a6898d97a6d23025ea42ff83
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-12-27 12:10:13 +0900

    모두0으로만들기 : bfs -완-

--- ct/src/프로그래머스/모두0으로만들기/Solution.java
+++ ct/src/프로그래머스/모두0으로만들기/Solution.java
@@ -1,45 +1,60 @@
 package 프로그래머스.모두0으로만들기;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Solution {
-    public static long cnt = 0;
-    public static List<List<Integer>> tree = new ArrayList<>();
-    public static boolean[] vis;
-    public static  long[] arr;
     public long solution(int[] a, int[][] edges) {
+        return bfs(a, edges);
+    }
+
+    public long bfs(int[] a, int[][] edges){
         int n = a.length;
-        arr = new long[n];
-        vis = new boolean[n];
+        int[] in = new int[n];
+        List<List<Integer>> tree= new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        long[] arr = new long[n];
         long sum = 0;
         for (int i = 0; i < n; i++) {
-            arr[i] = a[i];
-            sum += arr[i];
-        }
-        if (sum != 0) return -1;
-        for (int i = 0; i < n; i++) {
             tree.add(new ArrayList<>());
+            sum += a[i];
+            arr[i] = a[i];
         }
-        for (int[] edge : edges) {
-            tree.get(edge[0]).add(edge[1]);
-            tree.get(edge[1]).add(edge[0]);
+        if(sum != 0) return -1;
+        for (int i = 0; i < edges.length; i++) {
+            tree.get(edges[i][0]).add(edges[i][1]);
+            tree.get(edges[i][1]).add(edges[i][0]);
+            in[edges[i][0]]++;
+            in[edges[i][1]]++;
         }
-        vis[0] = true;
-        dfs(0);
+        long count = 0;
+        //진입차수가 0인 노드들 추가
+        for (int i = 0; i < n; i++) {
+            if(in[i] == 1){
+                queue.add(i);
+            }
+        }
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            //현재 노드와 연겯돼있는 노드들의 진입차수를 -1
+            for (int i = 0; i < tree.get(node).size(); i++) {
+                int next = tree.get(node).get(i);
+                if(in[next]==0) continue;
+                in[next]--;
+                arr[next] += arr[node];
+                count += Math.abs(arr[node]);
+                in[node] = 0;
+                arr[node] = 0;
+                if(in[next] == 1){
+                    queue.add(next);
 
-        return cnt;
-    }
-    public long dfs(int edge) {
-        long curValue = arr[edge];
-        for (int i=0;  i < tree.get(edge).size() ; i++) {
-            int next = tree.get(edge).get(i);
-            if (vis[next]) continue;
-            vis[next] = true;
-            curValue += dfs(next);
+                }
+            }
         }
-        cnt += curValue > 0 ? curValue : -curValue;
-        return curValue;
+
+        return count;
     }
 
     public static void main(String[] args) {
