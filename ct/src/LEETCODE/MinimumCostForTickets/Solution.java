403b8bf1dd26164f71c1d3b4b770bae9e3e80569
RyuSungHyeon990604 <shryoo990604@gmail.com>
2024-12-31 16:08:23 +0900

    983. Minimum Cost For Tickets : dp -완- 수정 : set사용 X

--- ct/src/LEETCODE/MinimumCostForTickets/Solution.java
+++ ct/src/LEETCODE/MinimumCostForTickets/Solution.java
@@ -1,32 +1,29 @@
 package LEETCODE.MinimumCostForTickets;
 
-import java.util.HashSet;
-import java.util.Set;
 
 public class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int n = days.length;
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            set.add(days[i]);
-        }
         int[] dp = new int[days[n-1]+1];
-
+        int index = 0;
         for (int i = 1; i < dp.length; i++) {
-            if(!set.contains(i)){
-                dp[i] = dp[i-1];
-                continue;
+            if(i != days[index]){
+               dp[i] = dp[i-1];
+            }else{
+                index++;
+                int cost1 = dp[i-1] + costs[0];
+                int cost7 = costs[1];
+                int cost30 = costs[2];
+                if(i-7>=0){
+                    cost7 = dp[i-7] + costs[1];
+                }
+                if(i-30>=0){
+                    cost30 = dp[i-30] + costs[2];
+                }
+                dp[i] = Math.min(cost1, Math.min(cost7, cost30));
             }
-            int cost1 = dp[i-1] + costs[0];
-            int cost7 = costs[1];
-            int cost30 = costs[2];
-            if(i-7>=0){
-                cost7 = dp[i-7] + costs[1];
-            }
-            if(i-30>=0){
-                cost30 = dp[i-30] + costs[2];
-            }
-            dp[i] = Math.min(cost1, Math.min(cost7, cost30));
+
+
         }
 
         return dp[days[n-1]];
