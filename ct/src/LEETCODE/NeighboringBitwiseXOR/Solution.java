9f878068237d39db116ccc8f336a9dbc3101e168
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-01-17 16:06:55 +0900

    2683. Neighboring Bitwise XOR : bit manipulation -완-

---
+++ ct/src/LEETCODE/NeighboringBitwiseXOR/Solution.java
@@ -0,0 +1,11 @@
+package LEETCODE.NeighboringBitwiseXOR;
+
+public class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int xor =0;
+        for(int a : derived){
+            xor^=a;
+        }
+        return xor == 0;
+    }
+}
