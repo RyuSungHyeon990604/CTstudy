9d20e00bbf32c871581f0eeb954d14519e7045b2
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-06-05 16:41:38 +0900

    프로그래머스. 야근지수 v2 : array -완-

--- ct/src/프로그래머스/야근지수/Solution.java
+++ ct/src/프로그래머스/야근지수/Solution.java
@@ -2,40 +2,41 @@ package 프로그래머스.야근지수;
 import java.util.TreeMap;
 
 public class Solution {
+	private final int MAX_SIZE = 50001;
 	//야근 피로도 += 야근을 시작한 시점에서 남은 일의 작업량 ^ 2
 	public long solution(int n, int[] works) {
 		long answer = 0;
-		TreeMap<Integer, Integer> map = new TreeMap<>();
+		int[] arr = new int[MAX_SIZE];
+		int max = -1;
 		for (int work : works) {
-			map.put(work, map.getOrDefault(work, 0) + 1);
+			arr[work]++;
+			max = Math.max(max, work);
 		}
 
-		while(n > 0) {
-			int v = map.lastKey();
-			if(v == 0) return 0;
-
-			int count = map.get(v);
-			if(n >= count) {
-				map.put(v-1, map.getOrDefault(v-1, 0) + count);
-				map.remove(v);
-				n -= count;
+		for (int i = max; i > 0 && n > 0; i--) {
+			if(arr[i] > n) {
+				arr[i-1] += n;
+				arr[i] = arr[i] - n;
+				n = 0;
 			} else {
-				map.put(v, count - n);
-				map.put(v-1, map.getOrDefault(v-1, 0) + n);
-				n=0;
+				max = i - 1;
+				arr[i-1] += arr[i];
+				n -= arr[i];
 			}
 		}
 
-		for (Integer i : map.keySet()) {
-			answer += ((long) i * i) * map.get(i);
+		for (int i = 0; i <= max; i++) {
+			answer += arr[i] * ((long) i * i);
 		}
+
+
 
 		return answer;
 	}
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		long solution1 = solution.solution(3, new int[]{1, 1});
+		long solution1 = solution.solution(1, new int[]{2, 1,2});
 		System.out.println("solution1 = " + solution1);
 	}
 }
