6de56c9034fcf7c5ec43e5f29b2fcc6b6cbed456
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-08-13 13:26:52 +0900

    326. Power of Three : math - 완 -

---
+++ ct/src/LEETCODE/PowerofThree/Solution.java
@@ -0,0 +1,17 @@
+package LEETCODE.PowerofThree;
+
+public class Solution {
+	public boolean isPowerOfThree(int n) {
+		if(n <= 0) return false;
+		while(n % 3 == 0) {
+			n = n / 3;
+		}
+		return n==1;
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		boolean powerOfThree = s.isPowerOfThree(3);
+		System.out.println(powerOfThree);
+	}
+}
